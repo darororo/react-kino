764ccc196d2d9755da58bc2812ca368fedbb82be
Yem Daro <daroyem99@gmail.com>
2025-12-29 18:24:40 +0700

    show active tab element

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,40 @@
+import { icons } from '@/constants/icons'
+import { images } from '@/constants/images'
 import { Tabs } from 'expo-router'
 import React from 'react'
+import { Image, ImageBackground, Text, View } from 'react-native'
+
+const TabIcon = ({focused, icon, title}: any) => {
+    if(focused) {
+      return (
+          <>
+                  <ImageBackground 
+                    source={images.highlight}
+                    className='flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden'
+                  >
+                    <Image 
+                      source={icon}
+                      tintColor="#151312"
+                      className="size-5"
+                    />
+                    <Text>{title}</Text>
+                  </ImageBackground>
+    
+          </>  
+      )
+    }
+
+    return (
+      <View
+        className="size-full justify-center items-center mt-4 rounded-full"
+      >
+        <Image
+          source={icon} tintColor="#A8B5DB"
+          className='size-5'
+        />
+      </View>
+    )
+}
 
 const _Layout = () => {
   return (
@@ -9,6 +44,13 @@ const _Layout = () => {
         options={{
           title: 'Home',
           headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon 
+              focused={focused} 
+              icon={icons.home} 
+              title="Home"
+            />
+          )
         }}
       />
       <Tabs.Screen
@@ -16,6 +58,13 @@ const _Layout = () => {
         options={{
           title: 'Search',
           headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon 
+              focused={focused} 
+              icon={icons.search} 
+              title="Search"
+            />
+          ) 
         }}
       />
        <Tabs.Screen
@@ -23,6 +72,13 @@ const _Layout = () => {
         options={{
           title: 'Saved',
           headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon 
+              focused={focused} 
+              icon={icons.save} 
+              title="Saved"
+            />
+          ) 
         }}
       />
        <Tabs.Screen
@@ -30,6 +86,13 @@ const _Layout = () => {
         options={{
           title: 'Profile',
           headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon 
+              focused={focused} 
+              icon={icons.person} 
+              title="Profile"
+            />
+          ) 
         }}
       />
 
